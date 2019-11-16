877b4db96f5556ed1f12f17ec389fbb24425d82d
shankaranarayanan-it19 <54206070+shankaranarayanan-it19@users.noreply.github.com>
2019-11-16 09:18:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+def maxi(x,y):
+    if(x>y):
+        print(x,"is the greatest")
+    else:
+        print(y,"is the greatest")
+maxi(2,3)
+
